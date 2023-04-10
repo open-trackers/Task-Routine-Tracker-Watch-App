6a73bc9af7bbe2d7a3295befd41c633a5e3606bc
Reed Es <reedes@fastmail.com>
2023-04-09 19:17:01 -0600

    ongoing

---
+++ WidgetTRT/WidgetTRTBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetTRTBundle.swift
+//  WidgetTRT
+//
+//  Created by Reed Esau on 4/9/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetTRTBundle: WidgetBundle {
+    var body: some Widget {
+        WidgetTRT()
+    }
+}
